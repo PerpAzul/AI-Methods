19981d54a09719595f6bc67bb48c58e1c1730c4b
PerpAzul <felipe.lucas@outlook.com>
2025-11-07 20:07:46 +0100

    Robot can move and jump, with a third person camera and animations

--- AI-Methods/Assets/Scripts/PlayerMovement.cs
+++ AI-Methods/Assets/Scripts/PlayerMovement.cs
@@ -1,21 +1,102 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Vector2 moveInput;     // Stores the movement input from the player
-    public float moveSpeed = 5f;   // Speed multiplier for movement
+    [SerializeField] private float maximumSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float jumpSpeed;
+    [SerializeField] private float jumpButtonGracePeriod;
+    [SerializeField] private Transform cameraTransform;
+
+    private Vector2 moveInput; 
+    private CharacterController characterController;
+    private Animator animator;
+    private float ySpeed;
+    private float originalStepOffset;
+    private float? lastGroundedTime;
+    private float? jumpButtonPressedTime;
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        originalStepOffset = characterController.stepOffset;
+    }
     
     void Update()
     {
         Vector3 movementDirection = new Vector3(moveInput.x, 0, moveInput.y);
-        movementDirection.Normalize();
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
         
-        transform.Translate(movementDirection * moveSpeed * Time.deltaTime);
+        animator.SetFloat("Input Magnitude", inputMagnitude, 0.05f, Time.deltaTime);
+        
+        float moveSpeed = inputMagnitude * maximumSpeed;
+        movementDirection = Quaternion.AngleAxis(cameraTransform.rotation.eulerAngles.y, Vector3.up) *
+                            movementDirection;
+        movementDirection.Normalize();
+
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        if (characterController.isGrounded)
+        {
+            lastGroundedTime = Time.time;
+        }
+        
+        if (Time.time - lastGroundedTime <= jumpButtonGracePeriod)
+        {
+            characterController.stepOffset = originalStepOffset;
+            ySpeed = -0.5f;
+
+            if (Time.time - jumpButtonPressedTime <= jumpButtonGracePeriod)
+            {
+                ySpeed = jumpSpeed;
+                jumpButtonPressedTime = null;
+                lastGroundedTime = null;
+            }
+        }
+        else
+        {
+            characterController.stepOffset = 0;
+        }
+        
+        Vector3 velocity = movementDirection * moveSpeed;
+        velocity.y = ySpeed;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation =
+                Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime); 
+        }
     }
     
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+    
+    public void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            jumpButtonPressedTime = Time.time;   
+        }
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        if (hasFocus)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
