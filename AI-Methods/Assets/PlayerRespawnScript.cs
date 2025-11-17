03b0a9a00f30de3e62888d0e5ebda5234ded2ed8
JBrade <162108621+JBrade@users.noreply.github.com>
2025-11-17 16:10:29 +0100

    working on respawn

--- AI-Methods/Assets/PlayerRespawnScript.cs
+++ AI-Methods/Assets/PlayerRespawnScript.cs
@@ -2,19 +2,24 @@ using UnityEngine;
 
 public class PlayerRespawnScript : MonoBehaviour
 {
+
+    private Vector3 playerOriginPosition;
+    public GameObject player;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerOriginPosition = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.X))
+        if (Input.GetKeyDown(KeyCode.R))
         {
-            Debug.Log("space key was pressed");
-            transform.position = new Vector3(0.0f, 0.5f, 0.0f);
+            //Debug.Log("space key was pressed");
+            //Physics.SyncTransforms();
+            player.transform.position = playerOriginPosition;
         }
     }
 }
