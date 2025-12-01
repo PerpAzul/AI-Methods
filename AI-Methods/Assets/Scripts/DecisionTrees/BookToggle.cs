c46ccfd07c4eee06c1416d38522d9b6da93786bc
fichte <go69xas@mytum.de>
2025-12-01 21:40:59 +0100

    rest of changes for book ui

--- AI-Methods/Assets/Scripts/DecisionTrees/BookToggle.cs
+++ AI-Methods/Assets/Scripts/DecisionTrees/BookToggle.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using Unity.Cinemachine;
+using UnityEngine.UI;
 
 public class BookToggle : MonoBehaviour
 {
     public Canvas fullscreenCanvas;
     public CinemachineCamera myCamera;
+    BookUntoggle bookUntoggle;
+    public GameObject locked; 
 
     private bool isNear;
     void OnTriggerEnter(Collider other)
@@ -16,12 +19,19 @@ public class BookToggle : MonoBehaviour
     {
         isNear = false;
     }
+    
+    void Start()
+    {
+        bookUntoggle = GameObject.Find("Book UIs").transform.Find(fullscreenCanvas.name).GetComponent<BookUntoggle>();
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (isNear && Input.GetKeyDown(KeyCode.E))
         {
+            locked.SetActive(false);
+            bookUntoggle.isUnlocked = true;
             fullscreenCanvas.gameObject.SetActive(true);
             myCamera.gameObject.SetActive(false);
             Cursor.visible = true;
--- AI-Methods/Assets/Scripts/DecisionTrees/BookUntoggle.cs
+++ AI-Methods/Assets/Scripts/DecisionTrees/BookUntoggle.cs
@@ -6,12 +6,13 @@ public class BookUntoggle : MonoBehaviour
     [SerializeField] Canvas canvas;
     [SerializeField] CinemachineCamera cam;
 
+    public bool isUnlocked = false;
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.E) && canvas.gameObject.activeSelf)
         {
-            Debug.Log("deactivated");
             canvas.gameObject.SetActive(false);
             cam.gameObject.SetActive(true);
             Cursor.visible = false;
