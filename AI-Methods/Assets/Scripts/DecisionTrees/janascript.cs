44897871e83c1cb90e44b26735b0bd5506fba472
fichte <go69xas@mytum.de>
2025-12-04 15:48:09 +0100

    minor qol changes

---
+++ AI-Methods/Assets/Scripts/DecisionTrees/janascript.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System;
+
+public class janascript : MonoBehaviour
+{
+    System.Random r = new System.Random();
+
+    public GameObject center;
+    public GameObject cornerOne;
+    public GameObject cornerTwo;
+    public GameObject cornerThree;
+    public GameObject cornerFour;
+
+    public Material blueGlow;
+    public Material greenGlow;
+    public Material redGlow;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //nextPlatform = false;
+        center.GetComponent<Renderer>().material = blueGlow;
+        cornerOne.GetComponent<Renderer>().material = blueGlow;
+        cornerTwo.GetComponent<Renderer>().material = blueGlow;
+        cornerThree.GetComponent<Renderer>().material = blueGlow;
+        cornerFour.GetComponent<Renderer>().material = blueGlow;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        int glowclr = r.Next(2);
+
+        if (glowclr == 0)
+        {
+            center.GetComponent<Renderer>().material = greenGlow;
+            cornerOne.GetComponent<Renderer>().material = greenGlow;
+            cornerTwo.GetComponent<Renderer>().material = greenGlow;
+            cornerThree.GetComponent<Renderer>().material = greenGlow;
+            cornerFour.GetComponent<Renderer>().material = greenGlow;
+        }
+        else
+        {
+            center.GetComponent<Renderer>().material = redGlow;
+            cornerOne.GetComponent<Renderer>().material = redGlow;
+            cornerTwo.GetComponent<Renderer>().material = redGlow;
+            cornerThree.GetComponent<Renderer>().material = redGlow;
+            cornerFour.GetComponent<Renderer>().material = redGlow;
+
+        }
+    }
+}
