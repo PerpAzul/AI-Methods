157be923e926c02b0e7aff5b93e7d6641b1c4d14
fichte <go69xas@mytum.de>
2025-12-04 15:59:30 +0100

    qol

--- AI-Methods/Assets/Scripts/Interactables/Dialogue System/DialogueTrigger.cs
+++ AI-Methods/Assets/Scripts/Interactables/Dialogue System/DialogueTrigger.cs
@@ -5,9 +5,20 @@ public class DialogueTrigger : InteractableI
     public Dialogue dialogue;
     [SerializeField] private DialogueManager manager;
     [SerializeField] private PauseMenu pauseMenu;
+    [SerializeField] private GameObject arrow;
+    private bool hasInteracted = false;
 
     protected override void Interact()
     {
+        // for decision trees bot
+        if (arrow != null)
+        {
+            if (!hasInteracted)
+            {
+                hasInteracted = true;
+                arrow.SetActive(false);
+            }
+        }
         if (pauseMenu.isPaused)
         {
             return;
