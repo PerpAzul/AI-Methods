32c9c4759031353cf38199a683e398b1c1621653
Lennart Hesse <lennart.hesse@tum.de>
2026-01-16 13:30:48 +0100

    bug fixes: player falls out of lobby, roadmap level chooser

--- AI-Methods/Assets/Scripts/Lobby/LobbyMemory/PlayerTeleportOnSceneLoad.cs
+++ AI-Methods/Assets/Scripts/Lobby/LobbyMemory/PlayerTeleportOnSceneLoad.cs
@@ -6,21 +6,14 @@ public class PlayerTeleportOnSceneLoad : MonoBehaviour
     public Transform lobbyTeleporter;
     public GameObject tutorialCanvas;
 
-    void Awake() {
-        Debug.Log("Awake called");
-    }
-
     void Start()
     {
-        Debug.Log("Start called");
-        Debug.Log(VariableStore.IsLobbyTutorialFinished());
         GetComponent<CharacterController>().enabled = false;
         if (!VariableStore.IsLobbyTutorialFinished()) {
             transform.position = tutorialTeleporter.position;
             tutorialCanvas.SetActive(true);
         } else if (PlayerPositionMemory.lastPosition != Vector3.zero) {
             transform.position = PlayerPositionMemory.lastPosition;
-            Debug.Log("Set to position: " + transform.position);
             tutorialCanvas.SetActive(false);
         } else {
             transform.position = lobbyTeleporter.position;
